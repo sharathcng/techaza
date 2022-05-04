2da257deed6b58e1b9c88ae126f91ddf548bfc34
noeljohnn <noeljon97@gmail.com>
2022-05-04 13:08:21 +0530

    Added login page and signup page

--- techaza/account/urls.py
+++ techaza/account/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('login', views.Login.as_view(), name="login"),
+    path('signup', views.Signup.as_view(), name="signup"),
     path('logout', views.Logout.as_view(), name="logout"),
 ]
--- techaza/account/views.py
+++ techaza/account/views.py
@@ -1,16 +1,21 @@
+from msilib.schema import ListView
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.views.generic.edit import CreateView
 # Create your views here.
 
 
 class Login(LoginView):
+    template_name = "loginPage.html"
     def get_success_url(self):
         url = self.get_redirect_url()
         messages.success(self.request,"Hey, Buddy..! Welcome to STCS dashboard.")
         return reverse_lazy('home')
 
+class Signup(CreateView):
+    template_name = "signup.html"
 
 class Logout(LogoutView):
     def get_success_url(self):
